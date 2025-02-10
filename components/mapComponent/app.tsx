4f8839e81b88af408c0efae67c94f413ca6fdbbb
Tianqi Yao <yaotianqi6666@gmail.com>
2025-02-10 03:35:20 -0600

    try

--- components/mapComponent/app.tsx
+++ components/mapComponent/app.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { APIProvider } from "@vis.gl/react-google-maps";
+import dynamic from 'next/dynamic';
 
-import DrawingExample from "./drawing-example";
+
+// import DrawingExample from "./drawing-example";
+
+const DrawingExample = dynamic(() => import('./drawing-example'), { ssr: false });
+
 
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string;
 
